75516a59cdcefa67a167f2cbb03525c110eec936
Pibomeister <edy7picazo@gmail.com>
2016-12-08 21:22:09 -0600

    Fixed the port for HEROKU

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const port = process.argv.PORT || 5000;
+const port = process.env.PORT || 5000;
 const publicPath = path.join(__dirname, '../public/');
 
 app.use(express.static(publicPath));
